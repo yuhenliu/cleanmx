59aa65d0d71bd009b532fccb4ae5cd2e120250ff
zagorakis <zagorakis@localhost.janus>
2012-11-25 06:54:00 +0800

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-Download malware from clean-mx.de
--- clean-mx-day.sh
+++ clean-mx-day.sh
@@ -8,26 +8,25 @@ url=`echo "wget -qO - http://lists.clean-mx.com/pipermail/viruswatch/$day/thread
 awk '/\[Virus/'|tail -n 1|sed 's:\": :g' |\
 awk '{print \"http://lists.clean-mx.com/pipermail/viruswatch/$day/\"$3}'"|sh`
 
+if test ! -d $rootdir/$day;then
+	mkdir $rootdir/$day
+fi
+cd $rootdir/$day
+
 wget -qO - http://lists.clean-mx.com/pipermail/viruswatch/$day/thread.html | awk '/\[Virus/' |\
 while read LINE
 do
 	filename=`echo $LINE | sed 's/"/ /g' | awk '{print $3}'`
-	echo $filename
-	links -dump $url$filename | awk '/Up/'|grep "TR\|exe" | awk '{print $2,$8,$10,$11,$12"\n"}' > $rootdir/$filename
-
 	dirname=`echo $filename |sed 's:.html::g'`
 
-	rm -rf $rootdir/$dirname
-	mkdir $rootdir/$dirname
+	if test ! -d $rootdir/$day/$dirname;then
+		mkdir $rootdir/$day/$dirname
+	fi
+	cd $rootdir/$day/$dirname
 
-	cd $rootdir
+	echo "Crawling $url$filename...................................................................................."
+	links -dump $url$filename | awk '/Up/'|grep "TR\|exe" | awk '{print $2,$8,$10,$11,$12"\n"}' > $filename
 
-	grep "exe$" $filename |awk '{print $5}'|sed 's/^\[.*\]//g' | xargs wget -t 10
-
+	grep "exe$" $filename |awk '{print $5}'|sed 's/^\[.*\]//g' | xargs wget -c -nc -t 5
 	ls *.exe | xargs md5sum >> checksums
-	mv *.exe $dirname
-
-	rm -r $rootdir/*exe*
-	mv checksums $rootdir/$dirname
-	mv $filename $rootdir/$dirname
 done
